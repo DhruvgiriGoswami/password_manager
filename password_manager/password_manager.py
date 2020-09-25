2304e0e5e19fa1270512f9aa63ab94924a94daf8
DhruvgiriGoswami <dhruvgiri27@gmail.com>
2020-09-25 14:51:53 +0530

    commented out the hi button and added spaces

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -12,6 +12,7 @@ locker_pic.grid(row=0, column=1)
 image1 = PhotoImage(file='..\images\lockerpic.png')
 locker_pic.create_image(20,20, anchor=NW, image=image1)
 
+
 L1 = Label(tk, text="User Name").grid(row=1, column=0)
 username_entry = Entry(tk, bd =5)
 username_entry.grid(row=1, column=1)
@@ -29,12 +30,12 @@ def openregister():
     regwindow.geometry("250x200")
 
     Label(regwindow,text="Username").grid(row=2, column=0)
-    username_entry = Entry(regwindow, bd=5)
-    username_entry.grid(row=2, column=1)
+    new_username_entry = Entry(regwindow, bd=5)
+    new_username_entry.grid(row=2, column=1)
 
     Label(regwindow, text="Password").grid(row=3, column=0)
-    password_entry = Entry(regwindow, bd=5)
-    password_entry.grid(row=3, column=1)
+    new_password_entry = Entry(regwindow, bd=5)
+    new_password_entry.grid(row=3, column=1)
 
     register_button = Button(regwindow, text="REGISTER")
     register_button.grid(row=6, column=1)
@@ -42,12 +43,13 @@ def openregister():
 register_button = Button(tk, text ="REGISTER",command=openregister)
 register_button.grid(row=4, column=1)
 
-def click():
-    hello = "hello " + username_entry.get()
-    mylabel = Label(tk, text = hello)
-    mylabel.grid(row=5, column=1)
-mybutton = Button(tk, text= "hi", command= click) 
-mybutton.grid(row=6, column=1)
+# def click():
+#     hello = "hello " + username_entry.get()
+#     mylabel = Label(tk, text = hello)
+#     mylabel.grid(row=5, column=1)
+# mybutton = Button(tk, text= "hi", command= click) 
+# mybutton.grid(row=6, column=1)
+
 
 username = username_entry.get()
 password = password_entry.get()
